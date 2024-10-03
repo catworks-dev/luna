770846e57608f0ea5e16c83ab9200b5ecab92300
gierr7976 <gierr7976@gmail.com>
2024-10-03 22:09:56 +0300

    feat(#5): Initial cobra config

---
+++ packages/session/cmd/root.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"catworks/luna/session/internal/config"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "session",
+	Short: "Session service for Luna app",
+	Run: func(cmd *cobra.Command, args []string) {
+		configPath := cmd.Flag("config").Value.String()
+		config := config.RequireConfig(configPath)
+
+		fmt.Printf("%+v\n", config)
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringP("config", "c", "config.yaml", "config file (default is config.yaml)")
+}
---
+++ packages/session/internal/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	StoragePath string        `yaml:"storage_path" env-required:"true"`
+	SessionTTL  time.Duration `yaml:"session_ttl" env-default:"2160h"`
+	Grpc        GrpcConfig    `yaml:"grpc"`
+}
+
+type GrpcConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func RequireConfig(path string) *Config {
+	if path == "" {
+		panic("Config path must not be empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("Config file not found")
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("Unable to read config file: " + err.Error())
+	}
+
+	return &cfg
+}
