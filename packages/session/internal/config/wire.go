098154383cc517a9f203cf7ce4d2b6ff3a93e97a
da-cat7976 <gierr7976@gmail.com>
2024-10-04 18:58:58 +0300

    feat(#5): Gorm initial

--- packages/session/internal/config/wire.go
+++ packages/session/internal/config/wire.go
@@ -5,17 +5,20 @@ package config
 import (
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 )
 
 type Container struct {
 	Config *Config
 	Logger *logrus.Logger
+	DB     *gorm.DB
 }
 
 func NewContainer(cfg *Config) (*Container, error) {
 	panic(
 		wire.Build(
 			NewLogger,
+			NewGorm,
 			wire.Struct(new(Container), "*"),
 		),
 	)
